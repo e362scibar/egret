a50dfff475345983ac9377e7646fd0007ae54238
Hirokazu Maesaka <maesaka@spring8.or.jp>
2025-10-23 23:50:19 +0900

    adding eigemode to envelope

--- egret/envelope.py
+++ egret/envelope.py
@@ -19,6 +19,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from __future__ import annotations
+from unittest import case
 
 import numpy as np
 import numpy.typing as npt
@@ -28,21 +29,21 @@ class Envelope:
     Beam envelope object.
     '''
 
-    def __init__(self, cov: npt.NDArray[np.floating] = np.eye(4), s: float = 0.):
+    def __init__(self, cov: npt.NDArray[np.floating] = np.eye(4), s: float = 0., T: npt.NDArray[np.floating] = None):
         '''
         Args:
             cov npt.NDArray[np.floating]: 4x4 positive-definite covariance matrix with the determinant of unity.
             s float: Longitudinal position [m].
+            T npt.NDArray[np.floating]: 2x2 coordinate transformation matrix for eigenmode. (Optional)
         '''
-        self.cov = cov.copy()
-        self.s = s
+        self.set(cov, s, T)
 
     def __getitem__(self, key):
         '''
         Get beta function value by key.
 
         Args:
-            key str: Key of the beta function. 'bx', 'ax', 'gx', 'by', 'ay', or 'gy'.
+            key str: Key of the beta function. 'bx', 'ax', 'gx', 'by', 'ay', 'gy', 'bu', 'au', 'gu', 'bv', 'av', 'gv', or 's'.
 
         Returns:
             float: Value of the beta function corresponding to the key.
@@ -60,44 +61,65 @@ class Envelope:
                 return -0.5 * (self.cov[2, 3] + self.cov[3, 2])
             case 'gy':
                 return self.cov[3, 3]
+            case 'bu':
+                return self.U[0, 0]
+            case 'au':
+                return -0.5 * (self.U[0, 1] + self.U[1, 0])
+            case 'gu':
+                return self.U[1, 1]
+            case 'bv':
+                return self.V[0, 0]
+            case 'av':
+                return -0.5 * (self.V[0, 1] + self.V[1, 0])
+            case 'gv':
+                return self.V[1, 1]
             case 's':
                 return self.s
             case _:
                 raise KeyError(f'Invalid key: {key}')
 
-    def __setitem__(self, key, value):
+    def set(self, cov: npt.NDArray[np.floating], s: float, T: npt.NDArray[np.floating] = None):
         '''
-        Set beta function value by key.
+        Set envelope parameters.
 
         Args:
-            key str: Key of the beta function. 'bx', 'ax', 'gx', 'by', 'ay', or 'gy'.
-            value float: Value to set.
+            cov npt.NDArray[np.floating]: 4x4 positive-definite covariance matrix with the determinant of unity.
+            s float: Longitudinal position [m].
+            T npt.NDArray[np.floating]: 4x4 coordinate transformation matrix for eigenmode. (Optional)
         '''
-        match key:
-            case 'bx':
-                self.cov[0, 0] = value
-                self.cov[1, 1] = (1. + self.cov[1, 0] * self.cov[0, 1]) / value
-            case 'ax':
-                self.cov[0, 1] = -value
-                self.cov[1, 0] = -value
-                self.cov[1, 1] = (1. + value**2) / self.cov[0, 0]
-            case 'gx':
-                self.cov[1, 1] = value
-                self.cov[0, 0] = (1. + self.cov[1, 0] * self.cov[0, 1]) / value
-            case 'by':
-                self.cov[2, 2] = value
-                self.cov[3, 3] = (1. + self.cov[3, 2] * self.cov[2, 3]) / value
-            case 'ay':
-                self.cov[2, 3] = -value
-                self.cov[3, 2] = -value
-                self.cov[3, 3] = (1. + value**2) / self.cov[2, 2]
-            case 'gy':
-                self.cov[3, 3] = value
-                self.cov[2, 2] = (1. + self.cov[3, 2] * self.cov[2, 3]) / value
-            case 's':
-                self.s = value
-            case _:
-                raise KeyError(f'Invalid key: {key}')
+        self.cov = cov.copy()
+        self.s = s
+        self.calc_eigenmode(T)
+
+    def calc_eigenmode(self, T: npt.NDArray[np.floating] = None):
+        '''
+        Calculate eigenmode transformation matrix.
+
+        Args:
+            T npt.NDArray[np.floating]: 4x4 coordinate transformation matrix for eigenmode. (Optional)
+        '''
+        Sxx, Sxy, Syy = self.cov[0:2, 0:2], self.cov[0:2, 2:4], self.cov[2:4, 2:4]
+        if T is not None:
+            self.T = T.copy()
+        else:
+            # Calculate T from the covariance matrix
+            mat = np.array([[-Sxx[0,0], -Sxx[0,1]-Syy[0,1], 0., Syy[0,0]],
+                            [0., -Syy[1,1], -Sxx[0,0], -Sxx[0,1]+Syy[0,1]],
+                            [-Sxx[0,1]+Syy[0,1], -Sxx[1,1], -Syy[0,0], 0.],
+                            [Syy[1,1], 0., -Sxx[0,1]-Syy[0,1], -Sxx[1,1]]])
+            vec = Sxy.reshape(4)
+            try:
+                res = np.linalg.solve(mat, vec)
+            except np.linalg.LinAlgError:
+                res = np.zeros(4)
+            T = res.reshape(2,2)
+            self.T = T
+        T_ = np.array([[T[1,1], -T[0,1]], [-T[1,0], T[0,0]]])
+        tau = np.sqrt(1. - np.linalg.det(T))
+        chi = 1. / (2. * tau**2 - 1.)
+        sqrtchi = np.sqrt(chi)
+        self.U = sqrtchi * (tau**2 * Sxx - T_ @ Syy @ T_.T)
+        self.V = sqrtchi * (tau**2 * Syy - T @ Sxx @ T.T)
 
     def copy(self) -> Envelope:
         '''
@@ -106,4 +128,4 @@ class Envelope:
         Returns:
             Envelope: A copy of the envelope object.
         '''
-        return Envelope(self.cov, self.s)
+        return Envelope(self.cov, self.s, self.T)
--- egret/envelopearray.py
+++ egret/envelopearray.py
@@ -28,21 +28,21 @@ class EnvelopeArray:
     Beam envelope array.
     '''
 
-    def __init__(self, cov: npt.NDArray[np.floating], s: npt.NDArray[np.floating]):
+    def __init__(self, cov: npt.NDArray[np.floating], s: npt.NDArray[np.floating], T: npt.NDArray[np.floating] = None):
         '''
         Args:
             cov npt.NDArray[np.floating]: 4x4xN positive-definite covariance matrices with the determinant of unity.
             s npt.NDArray[np.floating]: Longitudinal positions [m] with shape (N,).
+            T npt.NDArray[np.floating]: 2x2xN coordinate transformation matrices for eigenmode. (Optional)
         '''
-        self.cov = cov.copy()
-        self.s = s.copy()
+        self.set(cov, s, T)
 
     def __getitem__(self, key: str) -> npt.NDArray[np.floating]:
         '''
         Get beam envelope value by key.
 
         Args:
-            key str: Key of the coordinate. 'bx', 'ax', 'gx', 'by', 'ay', 'gy', or 's'.
+            key str: Key of the coordinate. 'bx', 'ax', 'gx', 'by', 'ay', 'gy', 'bu', 'au', 'gu', 'bv', 'av', 'gv', or 's'.
 
         Returns:
             NDArray: Value of the coordinate corresponding to the key.
@@ -60,51 +60,74 @@ class EnvelopeArray:
                 return -0.5 * (self.cov[2, 3, :] + self.cov[3, 2, :])
             case 'gy':
                 return self.cov[3, 3, :]
+            case 'bu':
+                return self.U[0, 0, :]
+            case 'au':
+                return -0.5 * (self.U[0, 1, :] + self.U[1, 0, :])
+            case 'gu':
+                return self.U[1, 1, :]
+            case 'bv':
+                return self.V[0, 0, :]
+            case 'av':
+                return -0.5 * (self.V[0, 1, :] + self.V[1, 0, :])
+            case 'gv':
+                return self.V[1, 1, :]
             case 's':
                 return self.s
             case _:
                 raise KeyError(f'Invalid key: {key}')
 
-    def __setitem__(self, key: str, value: float) -> None:
+    def set(self, cov: npt.NDArray[np.floating], s: npt.NDArray[np.floating], T: npt.NDArray[np.floating] = None) -> None:
         '''
-        Set coordinate value by key.
+        Set beam envelope parameters.
 
         Args:
-            key str: Key of the coordinate. 'bx', 'ax', 'gx', 'by', 'ay', 'gy', or 's'.
-            value NDArray: Value to set.
+            cov npt.NDArray[np.floating]: 4x4xN positive-definite covariance matrices with the determinant of unity.
+            s npt.NDArray[np.floating]: Longitudinal positions [m] with shape (N,).
+            T npt.NDArray[np.floating]: 2x2xN coordinate transformation matrices for eigenmode. (Optional)
         '''
-        match key:
-            case 'bx':
-                self.cov[0, 0, :] = value
-                self.cov[1, 1, :] = (1. + self.cov[1, 0, :] * self.cov[0, 1, :]) / value
-            case 'ax':
-                self.cov[0, 1, :] = -value
-                self.cov[1, 0, :] = -value
-                self.cov[1, 1, :] = (1. + value**2) / self.cov[0, 0, :]
-            case 'gx':
-                self.cov[1, 1, :] = value
-                self.cov[0, 0, :] = (1. + self.cov[1, 0, :] * self.cov[0, 1, :]) / value
-            case 'by':
-                self.cov[2, 2, :] = value
-                self.cov[3, 3, :] = (1. + self.cov[3, 2, :] * self.cov[2, 3, :]) / value
-            case 'ay':
-                self.cov[2, 3, :] = -value
-                self.cov[3, 2, :] = -value
-                self.cov[3, 3, :] = (1. + value**2) / self.cov[2, 2, :]
-            case 'gy':
-                self.cov[3, 3, :] = value
-                self.cov[2, 2, :] = (1. + self.cov[3, 2, :] * self.cov[2, 3, :]) / value
-            case 's':
-                self.s[:] = value
-            case _:
-                raise KeyError(f'Invalid key: {key}')
+        self.cov = cov.copy()
+        self.s = s.copy()
+        self.calc_eigenmode(T)
+
+    def calc_eigenmode(self, T: npt.NDArray[np.floating] = None) -> None:
+        '''
+        Calculate eigenmode.
+
+        Args:
+            T npt.NDArray[np.floating]: 2x2xN coordinate transformation matrices for eigenmode. (Optional)
+        '''
+        Sxx, Sxy, Syy = self.cov[0:2, 0:2, :], self.cov[0:2, 2:4, :], self.cov[2:4, 2:4, :]
+        if T is not None:
+            self.T = T.copy()
+        else:
+            N = self.cov.shape[2]
+            self.T = np.zeros((2, 2, N), dtype=self.cov.dtype)
+            # Calculate T from the covariance matrix
+            mat = np.array([[-Sxx[0,0,:], -Sxx[0,1,:]-Syy[0,1,:], 0., Syy[0,0,:]],
+                            [0., -Syy[1,1,:], -Sxx[0,0,:], -Sxx[0,1,:]+Syy[0,1,:]],
+                            [-Sxx[0,1,:]+Syy[0,1,:], -Sxx[1,1,:], -Syy[0,0,:], 0.],
+                            [Syy[1,1,:], 0., -Sxx[0,1,:]-Syy[0,1,:], -Sxx[1,1,:]]]).transpose(2, 0, 1)  # (N, 4, 4)
+            vec = Sxy.reshape(4, N).T  # (N, 4)
+            try:
+                res = np.linalg.solve(mat, vec)
+            except np.linalg.LinAlgError:
+                res = np.zeros((N, 4))
+            T = res.T.reshape(2, 2, N)
+            self.T = T
+        T_ = np.array([[T[1,1,:], -T[0,1,:]], [-T[1,0,:], T[0,0,:]]])
+        tau = np.sqrt(1. - np.linalg.det(T.transpose(2,0,1)))
+        chi = 1. / (2. * tau**2 - 1.)
+        sqrtchi = np.sqrt(chi)
+        self.U = sqrtchi * (tau**2 * Sxx - np.matmul(T_.transpose(2, 0, 1), np.matmul(Syy.transpose(2, 0, 1), T_.transpose(2, 0, 1)))).transpose(1,2,0)
+        self.V = sqrtchi * (tau**2 * Syy - np.matmul(T.transpose(2, 0, 1), np.matmul(Sxx.transpose(2, 0, 1), T.transpose(2, 0, 1)))).transpose(1,2,0)
 
     def copy(self) -> EnvelopeArray:
         '''
         Returns:
             EnvelopeArray: A copy of the envelope array object.
         '''
-        return EnvelopeArray(self.cov, self.s)
+        return EnvelopeArray(self.cov, self.s, self.T)
 
     def append(self, evlp: EnvelopeArray) -> None:
         '''
