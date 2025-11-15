dbc2f322112ddf91fc948964287279a8f21424c0
Hirokazu Maesaka <maesaka@spring8.or.jp>
2025-11-15 22:10:33 +0900

    Add GPLv3 license headers to C++ sources/headers and small fixes
    - Insert GPLv3-style header into C++ headers and sources under cpp/include and cpp/src
    - Fix stray character in dipole.cpp header
    
    (Added by automated edit)

--- cpp/src/cli_quadrupole.cpp
+++ cpp/src/cli_quadrupole.cpp
@@ -1,3 +1,23 @@
+// cli_quadrupole.cpp
+//
+// Copyright (C) 2025 Hirokazu Maesaka (RIKEN SPring-8 Center)
+//
+// This file is part of Egret: Engine for General Research in
+// Energetic-beam Tracking.
+//
+// Egret is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <iostream>
 #include <Eigen/Dense>
 #include "egret/quadrupole.hpp"
